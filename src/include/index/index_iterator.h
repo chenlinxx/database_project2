b30e10e1e24abe75f6bf3ddd607a57cd5dc5bbc6
陈林 <97414572+chenlinxx@users.noreply.github.com>
2022-12-09 19:12:06 +0800

    database_project2

---
+++ src/include/index/index_iterator.h
@@ -0,0 +1,37 @@
+﻿/**
+ * index_iterator.h
+ * For range scan of b+ tree
+ */
+#pragma once
+#include "page/b_plus_tree_leaf_page.h"
+
+namespace scudb {
+
+#define INDEXITERATOR_TYPE                                                     \
+  IndexIterator<KeyType, ValueType, KeyComparator>
+
+INDEX_TEMPLATE_ARGUMENTS
+class IndexIterator {
+public:
+  // you may define your own constructor based on your member variables
+  IndexIterator(B_PLUS_TREE_LEAF_PAGE_TYPE *leaf, int index, BufferPoolManager *bpm);
+  ~IndexIterator();
+
+  bool isEnd() {
+    return (leaf_ == nullptr) || (index_ >= leaf_->GetSize());
+  };
+
+  const MappingType &operator*() {
+    return leaf_->GetItem(index_);
+  };
+
+  IndexIterator &operator++();
+
+private:
+  // add your own private member variables here
+  B_PLUS_TREE_LEAF_PAGE_TYPE *leaf_;
+  int index_;
+  BufferPoolManager *bmp_;
+};
+
+} // namespace cmudb
